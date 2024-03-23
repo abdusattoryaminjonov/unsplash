a265c24aa58b849a502361e54ad94e5f20abce15
Xolmatov Foziljon <xolmatovfozil9@gmail.com>
2024-03-23 23:43:11 +0300

    saved function is added in DetailsPage

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,6 +1,4 @@
-
 import 'dart:typed_data';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
@@ -14,75 +12,76 @@ class DetailsPage extends StatefulWidget {
   final Result? imageModell;
   final PreviewPhoto? previewPhoto;
 
-  const DetailsPage({super.key, this.imageModell,this.previewPhoto});
+  const DetailsPage({Key? key, this.imageModell, this.previewPhoto}) : super(key: key);
 
   @override
   State<DetailsPage> createState() => _DetailsPageState();
 }
 
 class _DetailsPageState extends State<DetailsPage> {
-
-  String username ="";
+  String username = "";
   String urlImg = "";
 
-   _dialogsaveImage(){
+  _dialogsaveImage() {
     showDialog(
-        context: context,
-        builder: (BuildContext context){
-          return Container(
-              child: AlertDialog(
-                actions: [
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          actions: [
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 10),
                   Container(
-                    child:Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SizedBox(height: 10,),
-                        Container(
-                          padding: EdgeInsets.all(50),
-                          child: Lottie.asset("assets/images/downloade.json"),
-                        ),
-                        SizedBox(height: 10,),
-                        Text("Success!",style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),),
-                      ],
+                    padding: EdgeInsets.all(50),
+                    child: Lottie.asset("assets/images/downloade.json"),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Success!",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
                     ),
-                  )
+                  ),
                 ],
-              )
-          );
-        }
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 
-  late Uint8List _imageBytes;
+  late Uint8List _imageBytes = Uint8List(0);
 
   Future<void> loadImage() async {
     var response = await http.get(Uri.parse(urlImg));
-
     setState(() {
       _imageBytes = response.bodyBytes;
     });
-
   }
 
   Future<void> _downloadImage(Uint8List url) async {
-    bool result = await ImageGallerySaver.saveImage(url);
+    if (_imageBytes.isNotEmpty) {
+      bool result = await ImageGallerySaver.saveImage(url);
 
-    if (result){
-      _dialogsaveImage();
+      if (result) {
+        _dialogsaveImage();
+      }
+    } else {
+      print('Image bytes are empty, cannot download.');
+      // You can show a message or handle this case as needed.
     }
   }
 
-  _backToFinish(){
+  _backToFinish() {
     Navigator.of(context).pop(true);
   }
 
-
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     loadImage();
     username = widget.imageModell!.user.name;
@@ -92,20 +91,23 @@ class _DetailsPageState extends State<DetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        extendBodyBehindAppBar: true,
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         centerTitle: true,
-        title: Text(username,style: TextStyle(color: Colors.white),),
+        title: Text(
+          username,
+          style: TextStyle(color: Colors.white),
+        ),
         leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios,color: Colors.white,),
-            onPressed: () {
-              _backToFinish();
-            }
+          icon: Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () {
+            _backToFinish();
+          },
         ),
         actions: [
           IconButton(
-            onPressed: (){},
+            onPressed: () {},
             icon: Icon(Icons.ios_share),
             color: Colors.white,
           ),
@@ -113,7 +115,6 @@ class _DetailsPageState extends State<DetailsPage> {
       ),
       body: Column(
         children: [
-
           Expanded(
             child: Stack(
               children: [
@@ -121,8 +122,8 @@ class _DetailsPageState extends State<DetailsPage> {
                   child: new CachedNetworkImage(
                     imageUrl: urlImg,
                     placeholder: (context, url) => Image(
-                        image: AssetImage("assets/images/placeholder.png"),
-                        fit: BoxFit.cover
+                      image: AssetImage("assets/images/placeholder.png"),
+                      fit: BoxFit.cover,
                     ),
                     errorWidget: (context, url, error) => Icon(Icons.error),
                     height: MediaQuery.of(context).size.height,
@@ -134,34 +135,27 @@ class _DetailsPageState extends State<DetailsPage> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Container(
-                      padding: EdgeInsets.symmetric(vertical: 30,horizontal: 10),
+                      padding: EdgeInsets.symmetric(vertical: 30, horizontal: 10),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           IconButton(
-                            onPressed: (){},
-                            icon: Icon(Icons.info_outline,color: Colors.white,size: 30,),
+                            onPressed: () {},
+                            icon: Icon(Icons.info_outline, color: Colors.white, size: 30),
                           ),
                           IconButton(
-                            onPressed: (){
+                            onPressed: () {
                               _downloadImage(_imageBytes);
                             },
-                            icon: Icon(Icons.download_for_offline_outlined,color: Colors.white,size: 40,),
+                            icon: Icon(Icons.download_for_offline_outlined, color: Colors.white, size: 40),
                           ),
-                          // Container(
-                          //   margin: EdgeInsets.only(bottom: 30,right: 20),
-                          //   decoration: BoxDecoration(
-                          //     color: Colors.white,
-                          //     shape: BoxShape.circle,
-                          //   ),
-                          // )
                         ],
                       ),
                     ),
                   ],
                 )
               ],
-            )
+            ),
           ),
         ],
       ),
